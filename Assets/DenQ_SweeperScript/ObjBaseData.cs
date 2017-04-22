f07d5b15a5e8626e762c679ef4f8c980a0aa919e
lovelyapple <lovelyapple@gmail.com>
2017-04-22 15:48:25 +0900

    [1.0.0.0]最初の追加

---
+++ Assets/DenQ_SweeperScript/ObjBaseData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjBaseData : MonoBehaviour {
+	int MaxHp;
+	int RecHp;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
