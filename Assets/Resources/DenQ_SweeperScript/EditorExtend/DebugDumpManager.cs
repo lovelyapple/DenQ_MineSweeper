d9c451d99284ada558de281dbd26b51215ef45f4
lovelyapple <lovelyapple@gmail.com>
2017-05-22 22:18:37 +0900

    [1.1.0.0]エディター拡張：デバッグダンプマネージャー作成

---
+++ Assets/Resources/DenQ_SweeperScript/EditorExtend/DebugDumpManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using DenQ.Mgr;
+public class DebugDumpMangerWindow :EditorWindow
+{
+	
+}
+public class DebugDumpManager : MangerBase<DebugDumpManager>
+ {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
