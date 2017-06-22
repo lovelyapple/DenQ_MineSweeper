d7d92571878262c6b91ad69c293883b71df5ce9c
lovelyapple <lovelyapple@gmail.com>
2017-06-22 21:55:24 +0900

    [1.1.0.0]新規ボタンプレハブ作成1

---
+++ Assets/Resources/DenQ_SweeperScript/UI/Buttontest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttontest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void OnDebugClick(GameObject obj)
+	{
+		Debug.Log("buttonClicked");
+	}
+}
