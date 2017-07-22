68e8ae21bf3c2a89de2e643b476f994b5f048504
lovelyapple <lovelyapple@gmail.com>
2017-07-23 00:10:33 +0900

    [1.1.0.0]ObjectsId 適応

---
+++ Assets/Resources/DenQ_SweeperScript/System/GameObjectsManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DenQ.Mgr;
+
+public class GameObjectsManager : MangerBase<GameObjectsManager>
+{
+
+    //0スタート
+    private static long objectIndex = -1;
+    void Awake()
+    {
+        SetInstance(this);
+        ResetIdx();
+    }
+    public long RigistObjectId()
+    {
+        objectIndex++;
+        return objectIndex;
+    }
+    public void ResetIdx()
+    {
+        objectIndex = -1;
+    }
+}
