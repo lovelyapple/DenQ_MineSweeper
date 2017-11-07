3e1220b84b29eeb75a3f8f9074d023bca88d81fd
lovelyapple <lovelyapple@gmail.com>
2017-11-07 22:52:12 +0900

    [1.0.1.0]AIBase作成

---
+++ Assets/Resources/DenQ_SweeperScript/AI/AI_FieldBomb.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DenQData;
+
+public class AI_FieldBomb : AIBase 
+{
+	
+}
---
+++ Assets/Resources/DenQ_SweeperScript/System/Base/AIBase.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DenQData;
+
+public class AIBase : MonoBehaviour
+{
+    public FieldObjectData self { get; private set; }
+    public ActionController actionCtrl { get; private set; }
+    void Awake()
+    {
+        CatchSelfInfos();
+    }
+    public bool CatchSelfInfos()
+    {
+        self = gameObject.GetComponent<FieldObjectData>();
+
+        if (self == null)
+        {
+            Logger.GError("could not find FieldObject");
+            return false;
+        }
+
+        actionCtrl = gameObject.GetComponent<ActionController>();
+
+        if (actionCtrl == null)
+        {
+            Logger.GError("could not find actionController!");
+            return false;
+        }
+
+        return true;
+    }
+
+}
